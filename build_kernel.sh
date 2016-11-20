8e0e9d87d533ac0dd2ed35f58f7069bbc4eb276f
morogoku <morogoku@hotmail.com>
2016-11-20 21:10:21 +0100

    añadir build script y carpeta release Carpeta release donde se crearan los flasheable zip y tar

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,144 @@
 #!/bin/bash
 
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export ARCH=arm64
 
-make exynos7580-a5xelte_defconfig
-make -j
+# SETUP
+# -----
+
+export ARCH=arm64
+export SUBARCH=arm64
+export BUILD_CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$BUILD_CROSS_COMPILE
+export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
+
+export KERNEL_VERSION="ADM-Kernel-MoroTEST-v0.1"
+export KBUILD_BUILD_VERSION="1"
+
+KERNEL_DEFCONFIG=exynos7580-custom_defconfig
+
+RDIR=$(pwd)
+OUTDIR=$RDIR/arch/$ARCH/boot
+DTSDIR=$RDIR/arch/$ARCH/boot/dts
+DTBDIR=$OUTDIR/dtb
+DTCTOOL=$RDIR/scripts/dtc/dtc
+INCDIR=$RDIR/include
+PAGE_SIZE=2048
+DTB_PADDING=0
+
+
+
+# FUNCTIONS
+# ---------
+FUNC_DELETE_PLACEHOLDERS()
+{
+	find . -name \.placeholder -type f -delete
+        echo "Placeholders Deleted from Ramdisk"
+        echo ""
+}
+
+FUNC_CLEAN_DTB()
+{
+	if ! [ -d $RDIR/arch/$ARCH/boot/dts ] ; then
+		echo "no directory : "$RDIR/arch/$ARCH/boot/dts""
+	else
+		echo "rm files in : "$RDIR/arch/$ARCH/boot/dts/*.dtb""
+		rm $RDIR/arch/$ARCH/boot/dts/*.dtb
+		rm $RDIR/arch/$ARCH/boot/dtb/*.dtb
+		rm $RDIR/arch/$ARCH/boot/boot.img-dtb
+		rm $RDIR/arch/$ARCH/boot/boot.img-zImage
+	fi
+}
+
+FUNC_BUILD_KERNEL()
+{
+	echo ""
+        echo "build common config="$KERNEL_DEFCONFIG ""
+        echo "build variant config="$MODEL ""
+	FUNC_CLEAN_DTB
+	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
+			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
+			$KERNEL_DEFCONFIG || exit -1
+	make -j$BUILD_JOB_NUMBER ARCH=$ARCH \
+			CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
+	echo ""
+}
+
+FUNC_BUILD_DTB()
+{
+	[ -f "$DTCTOOL" ] || {
+		echo "You need to run ./build.sh first!"
+		exit 1
+	}
+
+	DTSFILES="exynos7580-a5xelte_eur_open_00 exynos7580-a5xelte_eur_open_01
+			exynos7580-a5xelte_eur_open_02 exynos7580-a5xelte_eur_open_03
+			exynos7580-a5xelte_eur_open_08 exynos7580-a5xelte_eur_open_09"
+
+	mkdir -p $OUTDIR $DTBDIR
+	cd $DTBDIR || {
+		echo "Unable to cd to $DTBDIR!"
+		exit 1
+	}
+	rm -f ./*
+	echo "Processing dts files."
+	for dts in $DTSFILES; do
+		echo "=> Processing: ${dts}.dts"
+		${CROSS_COMPILE}cpp -nostdinc -undef -x assembler-with-cpp -I "$INCDIR" "$DTSDIR/${dts}.dts" > "${dts}.dts"
+		echo "=> Generating: ${dts}.dtb"
+		$DTCTOOL -p $DTB_PADDING -i "$DTSDIR" -O dtb -o "${dts}.dtb" "${dts}.dts"
+	done
+	echo "Generating dtb.img."
+	$RDIR/tools/dtbtool -o "$OUTDIR/dtb.img" -p "$DTBDIR/" -s $PAGE_SIZE
+	echo "Done."
+}
+
+FUNC_BUILD_RAMDISK()
+{
+	mv $RDIR/arch/$ARCH/boot/Image $RDIR/arch/$ARCH/boot/boot.img-zImage
+	mv $RDIR/arch/$ARCH/boot/dtb.img $RDIR/arch/$ARCH/boot/boot.img-dtb
+
+	rm -f $RDIR/ramdisk/split_img/boot.img-zImage
+	rm -f $RDIR/ramdisk/split_img/boot.img-dtb
+	mv -f $RDIR/arch/$ARCH/boot/boot.img-zImage $RDIR/ramdisk/split_img/boot.img-zImage
+	mv -f $RDIR/arch/$ARCH/boot/boot.img-dtb $RDIR/ramdisk/split_img/boot.img-dtb
+	cd $RDIR/ramdisk
+	./repackimg.sh
+	echo SEANDROIDENFORCE >> image-new.img
+
+}
+
+FUNC_BUILD_FLASHABLES()
+{
+	cp image-new.img $RDIR/release/zip/boot.img
+	cp image-new.img $RDIR/release/tar/boot.img
+
+	cd $RDIR
+	cd release/zip
+	zip -5 -r $KERNEL_VERSION-$KBUILD_BUILD_VERSION.zip *
+	rm boot.img
+	cd ..
+	cd tar
+	tar cf $KERNEL_VERSION-$KBUILD_BUILD_VERSION.tar boot.img && ls -lh $KERNEL_VERSION-$KBUILD_BUILD_VERSION.tar
+	rm boot.img
+
+}
+
+# MAIN PROGRAM
+# ------------
+(
+	sh ./clean.sh
+	START_TIME=`date +%s`
+	FUNC_DELETE_PLACEHOLDERS
+	FUNC_BUILD_KERNEL
+	FUNC_BUILD_DTB
+	FUNC_BUILD_RAMDISK
+	FUNC_BUILD_FLASHABLES
+	END_TIME=`date +%s`
+	let "ELAPSED_TIME=$END_TIME-$START_TIME"
+	echo "Total compile time is $ELAPSED_TIME seconds"
+	echo ""
+) 2>&1	 | tee -a ./build.log
+
+	echo "Your flasheable release can be found in the release/zip or tar folder"
+	echo ""
+
+
